a37b9a1736115d56e8a510326766596b9db8eb4b
Robert Rodini <rrodini@hotmail.com>
2017-08-07 14:10:44 -0400

    to get port # from heroku run-time

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/player.js', function (req, res) {
 
 
 server = http.Server(app);
-server.listen(5000);
+server.listen(process.env.PORT || 5000);
 
 io = socketIO(server);
 let rooms = new Rooms(io);
